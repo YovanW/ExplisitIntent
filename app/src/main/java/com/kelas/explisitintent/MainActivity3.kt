2a98a5ca073d5452b6312f5b013a3be67e53ad8b
YovanW <yovanwisely@gmail.com>
2026-09-23 20:30:32 +0700

    topik 2 step 6

--- app/src/main/java/com/kelas/explisitintent/MainActivity3.kt
+++ app/src/main/java/com/kelas/explisitintent/MainActivity3.kt
@@ -1,6 +1,7 @@
 package com.kelas.explisitintent
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity3 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    companion object{
+        const val dataTerima = "extra_dataTerima"
     }
 }
